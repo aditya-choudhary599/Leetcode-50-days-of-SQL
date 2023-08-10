58a51e02260aa3e5d17d98cac737d84a927aa7cb
Aditya Choudhary <choudharyap21.comp@coep.ac.in>
2023-08-10 12:04:31 +0530

    Select Questions Added

---
+++ 01_Select/02_Find Customer Referee/01_ans.sql
@@ -0,0 +1,4 @@
+-- Question Link : https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50
+
+select name from Customer
+where referee_id!=2 or referee_id is NULL;
